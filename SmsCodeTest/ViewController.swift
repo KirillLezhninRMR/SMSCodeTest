c1314b7129dfa324ead2d908195651e3902a4291
Kirill Lezhnin <k.lezhnin@redmadrobot.com>
2021-06-25 20:14:02 +0500

    Initial Commit

---
+++ SmsCodeTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmsCodeTest
+//
+//  Created by Кирилл Лежнин on 25.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
